e6b4ea3320a742903492229b8044758ba9c68c44
iverly <github@iverly.net>
2023-11-21 12:30:59 +0100

    feat: add fastapi quickstart with pre-download gpt4all model
    Signed-off-by: iverly <github@iverly.net>

---
+++ main.py
@@ -0,0 +1,44 @@
+import logging
+import os
+
+from fastapi import FastAPI, HTTPException
+from starlette.middleware.cors import CORSMiddleware
+from gpt4all import GPT4All
+from api_v1 import events
+from api_v1.api import router as v1_router
+
+from settings import settings
+
+
+logger = logging.getLogger(__name__)
+app = FastAPI(title='GPT4All API', description="API for GPT4All following OpenAI specifications", version="0.0.1")
+
+# CORS Configuration (in-case you want to deploy)
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["*"],
+  allow_credentials=True,
+  allow_methods=["GET", "POST", "OPTIONS"],
+  allow_headers=["*"],
+)
+
+# Event handlers
+@app.on_event("startup")
+async def startup():
+  logger.info(f"Downloading/fetching model at {os.path.join(settings.gpt4all_path, settings.model)}")
+
+  # Download or fetch model
+  GPT4All(model_name=settings.model, model_path=settings.gpt4all_path)
+
+  logger.info(f"GPT4All API is ready to infer from {settings.model}, waiting for requests..")
+
+@app.on_event("shutdown")
+async def shutdown():
+  logger.info("Shutting down API")
+
+
+# https://github.com/tiangolo/fastapi/issues/2019
+LOG_FORMAT2 = (
+  "[%(asctime)s %(process)d:%(threadName)s] %(name)s - %(levelname)s - %(message)s |  %(filename)s:%(lineno)d"
+)
+logging.basicConfig(level=logging.INFO, format=LOG_FORMAT2)
---
+++ settings.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+
+class Settings(BaseSettings):
+    # General settings
+    app_environment = 'dev'
+    model: str = 'gpt4all-falcon-q4_0.gguf'
+    gpt4all_path: str = './models'
+
+    # GPT4All settings
+    temp: float = 0.18
+    top_p: float = 1.0
+    top_k: int = 50
+    repeat_penalty: float = 1.18
+
+settings = Settings()
